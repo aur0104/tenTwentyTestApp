1e2a1c5bcad53e438a80d81fdff578879d5e16f6
Ateeq <ateeq4112@gmail.com>
2025-10-08 03:29:55 +0500

    Add Poppins font family and SVG support
    Added all Poppins font variants to assets and linked them for both iOS and Android. Updated project configuration to support custom fonts and SVG imports, including metro and TypeScript settings. Installed react-native-svg and react-native-svg-transformer for SVG support.

---
+++ src/config/fonts.ts
@@ -0,0 +1,10 @@
+const FONT_FAMILIES = {
+  BOLD: "Poppins-Bold",
+  SEMI_BOLD: "Poppins-SemiBold",
+  MEDIUM: "Poppins-Medium",
+  REGULAR: "Poppins-Regular",
+};
+
+export default {
+  FONT_FAMILIES,
+};
